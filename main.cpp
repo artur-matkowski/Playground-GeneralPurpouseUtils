3efc0d3ec1831c41b352a2c306b12fd776686cea
Artur <artur.matkowski.zan@gmail.com>
2020-10-10 12:55:41 +0200

    CI fixes, asynchronous tests part1

--- main.cpp
+++ main.cpp
@@ -17,14 +17,36 @@ using namespace bfu;
 
 int main(int argc, char** argv)
 {
-	bool test = true;
+	if( arc < 2 )
+	{
+		cout << "\nUsage:";
+		cout << "\n\t" << argv[0] << " [1] [2]";
+		cout << "\n\t[1]:";
+		cout << "\n\t\t- json    - runs json tests";
+		cout << "\n\t\t- strema  - runs stream rests";
+		cout << "\n\t\t- logging - runs logging tests";
+		cout << "\n\t\t- udp     - runs udp tests";
+		cout << "\n\t\t- event   - event tests";
+
+		return -1;
+	}
+
+	if( strcmp(argv, "json") == 0 )
+	{
+		bool ret = jsonTests();
+
+		if(ret)
+			return 0;
+		else
+			return -1;
+	}
 
 	//log::Log.EnableFileLogging();
 	//streamTests();
 	//loggingTests();
 
 	
-	test = test && jsonTests();
+	jsonTests();
 	//udpTests(argc, argv);
 
 	//EventTest(argc, argv);
@@ -33,10 +55,15 @@ int main(int argc, char** argv)
 	//cout << "\nProject Name: " << getenv("PROJECT_NAME") << std::endl;
 
 
-	if(test)
-		return 0;
-	else
-		return -1;
+	cout << "\nUsage:";
+	cout << "\n\t" << argv[0] << " [1] [2]";
+	cout << "\n\t[1]:";
+	cout << "\n\t\t- json    - runs json tests";
+	cout << "\n\t\t- strema  - runs stream rests";
+	cout << "\n\t\t- logging - runs logging tests";
+	cout << "\n\t\t- udp     - runs udp tests";
+	cout << "\n\t\t- event   - event tests";
+	return -1;
 }
 
 
