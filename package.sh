05deccf795724e6cd395f650ca94c5155e3b4406
Artur <artur.matkowski.zan@gmail.com>
2020-10-11 20:56:53 +0200

    tweaked package name

--- package.sh
+++ package.sh
@@ -5,7 +5,7 @@ LOW_NAME="low"
 BUILD_NAME="build"
 HERE=${PWD##*/}
 
-PREFIX="Package: linuxconfig
+PREFIX="Package: bitforge test package
 \nVersion: "
 SUFFIX="Section: custom
 \nPriority: optional
