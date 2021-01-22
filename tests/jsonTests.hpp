17abf6ed73dd310de7aae5680f2b8b1b2905909d
Artur <artur.matkowski.zan@gmail.com>
2021-01-22 18:28:39 +0100

    json test uncommented, to mutch allocations

--- tests/jsonTests.hpp
+++ tests/jsonTests.hpp
@@ -23,7 +23,6 @@ bool randb()
 
 
 
-
 template<typename T>
 bool _TESTJSONStream(const char* _typename, const T& val, bfu::MemBlockBase* memBlock)
 {
@@ -58,6 +57,44 @@ bool _TESTJSONStream(const char* _typename, const T& val, bfu::MemBlockBase* mem
 		return false;		
 	}
 }
+
+
+template<>
+bool _TESTJSONStream<bfu::string>(const char* _typename, const bfu::string& val, bfu::MemBlockBase* memBlock)
+{
+
+	bfu::JSONStream json(memBlock);
+	bfu::JSONStream json2(memBlock);
+	bfu::SerializableVar<bfu::string> tt(_typename, 0, memBlock);
+	bfu::SerializableVar<bfu::string> tt2(_typename, 0, memBlock);
+	tt = val;
+
+	json << tt;
+
+	json.SetCursonPos(0);
+
+	json >> tt2;
+	json2 << tt2;
+
+	log::info << "Testing: " << _typename << "\n\tOriginal input:\n\t\t>" << tt 
+	 		<< "<\n\tSerialized to JSON:\n\t\t>" << json.str()  
+	 		<< "<\n\tDeserialized back to type:\n\t\t>" << tt2
+	 		<< "<\n\tSerialized to JSON2:\n\t\t>" << json2.str()  
+			<< "<\n" << std::endl;
+
+	if( std::strcmp(json.str().c_str(), json2.str().c_str() )==0 )
+	{
+		log::warning << "<<<<<<<<<<<<<<<< Test concluded : SUCCES\n" << std::endl;
+		return true;
+	}
+	else
+	{
+		log::error << "<<<<<<<<<<<<<<<< Test concluded : FAILED\n" << std::endl;
+		return false;		
+	}
+}
+
+
 
 
 #define TESTJSONStream(T,v, m) _TESTJSONStream<T>(#T, (T)v, m)
@@ -424,62 +461,62 @@ bool jsonTests( bfu::MemBlockBase* mBlock )
 
 
 	bool test = true;
-	/*
-	test = test && TESTJSONStream(float, randf() );
-	test = test && TESTJSONStream(float, randf() );
+	
+	test = test && TESTJSONStream(float, randf(), mBlock );
+	test = test && TESTJSONStream(float, randf(), mBlock );
 
 
-	test = test && TESTJSONStream(int, randi() );
-	test = test && TESTJSONStream(int, randi() );
+	test = test && TESTJSONStream(int, randi(), mBlock );
+	test = test && TESTJSONStream(int, randi(), mBlock );
 
-	test = test && TESTJSONStream(int64_t, randi() );
-	test = test && TESTJSONStream(int64_t, randi() );
+	test = test && TESTJSONStream(int64_t, randi(), mBlock );
+	test = test && TESTJSONStream(int64_t, randi(), mBlock );
 
-	test = test && TESTJSONStream(uint64_t, (randi()+50) );
-	test = test && TESTJSONStream(uint64_t, (randi()+50) );
+	test = test && TESTJSONStream(uint64_t, (randi()+50), mBlock );
+	test = test && TESTJSONStream(uint64_t, (randi()+50), mBlock );
 
-	test = test && TESTJSONStream(int32_t, randi() );
-	test = test && TESTJSONStream(int32_t, randi() );
+	test = test && TESTJSONStream(int32_t, randi(), mBlock );
+	test = test && TESTJSONStream(int32_t, randi(), mBlock );
 
-	test = test && TESTJSONStream(uint32_t, (randi()+50) );
-	test = test && TESTJSONStream(uint32_t, (randi()+50) );
+	test = test && TESTJSONStream(uint32_t, (randi()+50), mBlock );
+	test = test && TESTJSONStream(uint32_t, (randi()+50), mBlock );
 
-	test = test && TESTJSONStream(int16_t, randi() );
-	test = test && TESTJSONStream(int16_t, randi() );
+	test = test && TESTJSONStream(int16_t, randi(), mBlock );
+	test = test && TESTJSONStream(int16_t, randi(), mBlock );
 
-	test = test && TESTJSONStream(uint16_t, (randi()+50) );
-	test = test && TESTJSONStream(uint16_t, (randi()+50) );
+	test = test && TESTJSONStream(uint16_t, (randi()+50), mBlock );
+	test = test && TESTJSONStream(uint16_t, (randi()+50), mBlock );
 
-	test = test && TESTJSONStream(int8_t, randi() );
-	test = test && TESTJSONStream(int8_t, randi() );
+	test = test && TESTJSONStream(int8_t, randi(), mBlock );
+	test = test && TESTJSONStream(int8_t, randi(), mBlock );
 
-	test = test && TESTJSONStream(uint8_t, (randi()+50) );
-	test = test && TESTJSONStream(uint8_t, (randi()+50) );
-
-
-	test = test && TESTJSONStream(bool, randb() );
-	test = test && TESTJSONStream(bool, randb() );
+	test = test && TESTJSONStream(uint8_t, (randi()+50), mBlock );
+	test = test && TESTJSONStream(uint8_t, (randi()+50), mBlock );
 
 
-	test = test && TESTJSONStream(bfu::string, "gowno test \"213.ad,das");
+	test = test && TESTJSONStream(bool, randb(), mBlock );
+	test = test && TESTJSONStream(bool, randb(), mBlock );
+
+
+	test = test && TESTJSONStream(bfu::string, "gowno test \"213.ad,das", mBlock);
 
 
 
-	test = test && TESTJSONStreamVector(bool, randb(), randb(), randb(), randb()); 
+	test = test && TESTJSONStreamVector(bool, mBlock, randb(), randb(), randb(), randb()); 
 
-	test = test && TESTJSONStreamVector(float, randf(), randf(), randf(), randf() ); 
+	test = test && TESTJSONStreamVector(float, mBlock, randf(), randf(), randf(), randf() ); 
 
-	test = test && TESTJSONStreamVector(int, randi(), randi(), randi(), randi(), randi() ); 
+	test = test && TESTJSONStreamVector(int, mBlock, randi(), randi(), randi(), randi(), randi() ); 
 
-	test = test && TESTJSONStreamVector(bfu::string, "test 1", "test 2", "test 3", "test 4", "test 5" ); 
-*/
+	test = test && TESTJSONStreamVector(bfu::string, mBlock, "test 1", "test 2", "test 3", "test 4", "test 5" ); 
+
 
 	test = test && _TESTclass(mBlock);
 
 	test = test && _TESTclassNested(mBlock);
-/*
+
 	test = test && _TESTclassNestedJSON(mBlock);
-*/
+
 
 	if( test )
 	{
