a7635ea9d148dba245b9ecc583771eb2b9fb5f30
Artur <artur.matkowski.zan@gmail.com>
2021-03-17 18:26:10 +0100

    test generated from define

--- tests/ObjectSerializable_tests.hpp
+++ tests/ObjectSerializable_tests.hpp
@@ -23,60 +23,61 @@ namespace ObjectSerializationTests
 		return (rand() % 2) == 0;
 	}
 
-
-
-
-	class customSerializatorTest: public bfu2::SerializableClassBase<customSerializatorTest>
-	{
-	public:
-		SERIALIZABLE_VAR(customSerializatorTest, int, i);
-	public:
-		customSerializatorTest()
-		{};
-		~customSerializatorTest(){};
-	};
-
-
-	template<typename T>
-	bool _TESTJSONStream(const char* _typename, const T& val, bfu::MemBlockBase* memBlock)
-	{
-		char buff1[4096];
-		char buff2[4096];
-
-		bfu2::JSONSerializer serializer1(buff1, 4096, memBlock);
-		bfu2::JSONSerializer serializer2(buff2, 4096, memBlock);
-
-		customSerializatorTest tt;
-		customSerializatorTest tt2;
-		tt.i = val;
-
-		serializer1.Serialize(&tt);
-		//json << tt;
-
-		serializer1.SetCursonPos(0);
-
-		serializer1.Deserialize(&tt2);
-		//json >> tt2;
-		serializer2.Serialize(&tt2);
-		//json2 << tt2;
-
-		log::info << "Testing: " << _typename << "\n\tOriginal input:\n\t\t>" << tt.i
-		 		<< "<\n\tSerialized to JSON:\n\t\t>" << serializer1.str()  
-		 		<< "<\n\tDeserialized back to type:\n\t\t>" << tt2.i
-		 		<< "<\n\tSerialized to JSON2:\n\t\t>" << serializer2.str()  
-				<< "<\n" << std::endl;
-
-		if( std::strcmp(serializer1.str().c_str(), serializer2.str().c_str() )==0 && tt.i==tt2.i )
-		{
-			log::warning << "<<<<<<<<<<<<<<<< Test concluded : SUCCES\n" << std::endl;
-			return true;
-		}
-		else
-		{
-			log::error << "<<<<<<<<<<<<<<<< Test concluded : FAILED\n" << std::endl;
-			return false;		
-		}
+#define GENERATE_TEST_FOR_SIMPLE_VAR(T, value) \
+	namespace testing_##T \
+	{ \
+		class A: public bfu2::SerializableClassBase<A> \
+		{ \
+		public: \
+			SERIALIZABLE_VAR(A, T, i); \
+		public: \
+			A() \
+			{}; \
+			~A(){}; \
+		}; \
+		bool _TESTJSONStream(bfu::MemBlockBase* memBlock) \
+		{ \
+			char buff1[4096]; \
+			char buff2[4096]; \
+			 \
+			bfu2::JSONSerializer serializer1(buff1, 4096, memBlock); \
+			bfu2::JSONSerializer serializer2(buff2, 4096, memBlock); \
+			 \
+			A tt; \
+			A tt2; \
+			tt.i = value; \
+			 \
+			serializer1.Serialize(&tt); \
+			 \
+			serializer1.SetCursonPos(0); \
+			 \
+			serializer1.Deserialize(&tt2); \
+			serializer2.Serialize(&tt2); \
+			 \
+			log::info << "Testing: " << #T << "\n\tOriginal input:\n\t\t>" << tt.i \
+			 		<< "<\n\tSerialized to JSON:\n\t\t>" << serializer1.str()   \
+			 		<< "<\n\tDeserialized back to type:\n\t\t>" << tt2.i \
+			 		<< "<\n\tSerialized to JSON2:\n\t\t>" << serializer2.str()   \
+					<< "<\n" << std::endl; \
+					 \
+			if( std::strcmp(serializer1.str().c_str(), serializer2.str().c_str() )==0 && tt.i==tt2.i ) \
+			{ \
+				log::warning << "<<<<<<<<<<<<<<<< Test concluded : SUCCES\n" << std::endl; \
+				return true; \
+			} \
+			else \
+			{ \
+				log::error << "<<<<<<<<<<<<<<<< Test concluded : FAILED\n" << std::endl; \
+				return false;		 \
+			} \
+		} \
 	}
+
+#define PROCESS_TEST_FOR_SIMPLE_VAR(T, mBlock) \
+	testing_##T::_TESTJSONStream(mBlock)
+
+
+GENERATE_TEST_FOR_SIMPLE_VAR(int, randI() );
 
 	#define TTESTJSONStream(T,v, m) _TESTJSONStream<T>(#T, (T)v, m)
 
@@ -85,7 +86,7 @@ namespace ObjectSerializationTests
 	{
 		bool test = true;
 
-		test = test && TTESTJSONStream(int, 23, mBlock);
+		test = test && PROCESS_TEST_FOR_SIMPLE_VAR(int, mBlock);
 
 		//bfu2::SerializableClassInterface* p = new customSerializatorTest<float>();
 
